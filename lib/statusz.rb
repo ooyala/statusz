be57ca7292e29bed9f99396b7f4442d66c0c8aa5
Caleb Spare <caleb@ooyala.com>
2012-10-29 16:08:10 -0700

    Fix a small bug with text format.

--- lib/statusz.rb
+++ lib/statusz.rb
@@ -57,7 +57,7 @@ module Statusz
       # For readability, it's nice to have the massive 'all commits' field be the last one.
       all_commits = fields.delete "all commits"
       fields_with_commits_last = fields.map { |name, value| [name, value] }
-      fields_with_commits_last << all_commits if all_commits
+      fields_with_commits_last << ["all commits", all_commits] if all_commits
       fields_with_commits_last.map { |name, value| "#{name}:\n#{value}" }.join("\n\n")
     when :json
       fields.to_json
